e25263b382254f2530af2eca4d7f9caee35e5e2f
Regina Yashina <reginayashina@inbox.ru>
2022-05-06 14:02:58 +0300

    feat: add keyboard markup

---
+++ js/app.js
@@ -0,0 +1,706 @@
+let firstRowKeys = [
+	{
+		"key": "`",
+		"keyCode": 192,
+		"which": 192,
+		"code": "Backquote",
+		"location": 0,
+		"description": "Backtick / grave accent / ñ / æ / ö"
+	},
+	{
+		"key": "1",
+		"keyCode": 49,
+		"which": 49,
+		"code": "Digit1",
+		"location": 0,
+		"description": "1 Key",
+		"unicode": "①"
+	},
+	{
+		"key": "2",
+		"keyCode": 50,
+		"which": 50,
+		"code": "Digit2",
+		"location": 0,
+		"description": "2 Key",
+		"unicode": "②"
+	},
+	{
+		"key": "3",
+		"keyCode": 51,
+		"which": 51,
+		"code": "Digit3",
+		"location": 0,
+		"description": "3 Key",
+		"unicode": "③"
+	}, {
+		"key": "4",
+		"keyCode": 52,
+		"which": 52,
+		"code": "Digit4",
+		"location": 0,
+		"description": "4 Key",
+		"unicode": "④"
+	}, {
+		"key": "5",
+		"keyCode": 53,
+		"which": 53,
+		"code": "Digit5",
+		"location": 0,
+		"description": "5 Key",
+		"unicode": "⑤"
+	}, {
+		"key": "6",
+		"keyCode": 54,
+		"which": 54,
+		"code": "Digit6",
+		"location": 0,
+		"description": "6 Key",
+		"unicode": "⑥"
+	}, {
+		"key": "7",
+		"keyCode": 55,
+		"which": 55,
+		"code": "Digit7",
+		"location": 0,
+		"description": "7 Key",
+		"unicode": "⑦"
+	}, {
+		"key": "8",
+		"keyCode": 56,
+		"which": 56,
+		"code": "Digit8",
+		"location": 0,
+		"description": "8 Key",
+		"unicode": "⑧"
+	}, {
+		"key": "9",
+		"keyCode": 57,
+		"which": 57,
+		"code": "Digit9",
+		"location": 0,
+		"description": "9 Key",
+		"unicode": "⑨"
+	}, {
+		"key": "0",
+		"keyCode": 48,
+		"which": 48,
+		"code": "Digit0",
+		"location": 0,
+		"description": "0",
+		"unicode": "⓪"
+	}, {
+		"key": "-",
+		"keyCode": 109,
+		"which": 109,
+		"code": "Minus",
+		"location": 3,
+		"description": "subtract"
+	}, {
+		"key": "=",
+		"keyCode": 61,
+		"which": 61,
+		"code": "Equal",
+		"location": 0,
+		"description": "equals (firefox)"
+	}, {
+		"key": "Backspace",
+		"keyCode": 8,
+		"which": 8,
+		"code": "Backspace",
+		"location": 0,
+		"description": "backspace / delete",
+		"unicode": "⌫"
+	}
+];
+let secondRowKeys = [{
+	"key": "Tab",
+	"keyCode": 9,
+	"which": 9,
+	"code": "Tab",
+	"location": 0,
+	"description": "tab",
+	"unicode": "↹"
+}, {
+	"key": "q",
+	"keyCode": 81,
+	"which": 81,
+	"code": "KeyQ",
+	"location": 0,
+	"description": "q"
+}, {
+	"key": "w",
+	"keyCode": 87,
+	"which": 87,
+	"code": "KeyW",
+	"location": 0,
+	"description": "w"
+}, {
+	"key": "e",
+	"keyCode": 69,
+	"which": 69,
+	"code": "KeyE",
+	"location": 0,
+	"description": "e"
+}, {
+	"key": "r",
+	"keyCode": 82,
+	"which": 82,
+	"code": "KeyR",
+	"location": 0,
+	"description": "r"
+}, {
+	"key": "t",
+	"keyCode": 84,
+	"which": 84,
+	"code": "KeyT",
+	"location": 0,
+	"description": "t"
+},
+{
+	"key": "y",
+	"keyCode": 89,
+	"which": 89,
+	"code": "KeyY",
+	"location": 0,
+	"description": "y"
+},
+{
+	"key": "u",
+	"keyCode": 85,
+	"which": 85,
+	"code": "KeyU",
+	"location": 0,
+	"description": "u"
+},
+{
+	"key": "i",
+	"keyCode": 73,
+	"which": 73,
+	"code": "KeyI",
+	"location": 0,
+	"description": "i"
+},
+{
+	"key": "o",
+	"keyCode": 79,
+	"which": 79,
+	"code": "KeyO",
+	"location": 0,
+	"description": "o"
+},
+{
+	"key": "p",
+	"keyCode": 80,
+	"which": 80,
+	"code": "KeyP",
+	"location": 0,
+	"description": "p"
+}, {
+	"key": "[",
+	"keyCode": 160,
+	"which": 160,
+	"code": "BracketLeft",
+	"location": 0,
+	"altKey": false,
+	"ctrlKey": false,
+	"metaKey": false,
+	"shiftKey": false,
+	"description": "^"
+}, {
+	"key": "]",
+	"keyCode": 221,
+	"which": 221,
+	"code": "BracketRight",
+	"location": 0,
+	"description": "close bracket / å"
+},
+{
+	"key": "\\",
+	"keyCode": 220,
+	"which": 220,
+	"code": "Backslash",
+	"location": 0,
+	"description": "back slash"
+},
+{
+	"key": "Delete",
+	"keyCode": 46,
+	"which": 46,
+	"code": "Delete",
+	"location": 3,
+	"altKey": false,
+	"ctrlKey": false,
+	"metaKey": false,
+	"shiftKey": false,
+	"description": "delete",
+	"unicode": "⌦"
+}
+]
+let thirdRowKeys = [
+	{
+		"key": "CapsLock",
+		"keyCode": 20,
+		"which": 20,
+		"code": "CapsLock",
+		"location": 0,
+		"description": "caps lock",
+		"unicode": "⇪"
+	},
+	{
+		"key": "a",
+		"keyCode": 65,
+		"which": 65,
+		"code": "KeyA",
+		"location": 0,
+		"description": "a"
+	},
+	{
+		"key": "s",
+		"keyCode": 83,
+		"which": 83,
+		"code": "KeyS",
+		"location": 0,
+		"description": "s"
+	},
+	{
+		"key": "d",
+		"keyCode": 68,
+		"which": 68,
+		"code": "KeyD",
+		"location": 0,
+		"description": "d"
+	},
+	{
+		"key": "f",
+		"keyCode": 70,
+		"which": 70,
+		"code": "KeyF",
+		"location": 0,
+		"description": "f"
+	},
+	{
+		"key": "g",
+		"keyCode": 71,
+		"which": 71,
+		"code": "KeyG",
+		"location": 0,
+		"description": "g"
+	},
+	{
+		"key": "h",
+		"keyCode": 72,
+		"which": 72,
+		"code": "KeyH",
+		"location": 0,
+		"description": "h"
+	},
+	{
+		"key": "j",
+		"keyCode": 74,
+		"which": 74,
+		"code": "KeyJ",
+		"location": 0,
+		"description": "j"
+	},
+	{
+		"key": "k",
+		"keyCode": 75,
+		"which": 75,
+		"code": "KeyK",
+		"location": 0,
+		"altKey": true,
+		"ctrlKey": false,
+		"metaKey": true,
+		"shiftKey": false,
+		"description": "k"
+	},
+	{
+		"key": "l",
+		"keyCode": 76,
+		"which": 76,
+		"code": "KeyL",
+		"location": 0,
+		"altKey": true,
+		"ctrlKey": false,
+		"metaKey": true,
+		"shiftKey": false,
+		"description": "l"
+	}, {
+		"key": ";",
+		"keyCode": 59,
+		"which": 59,
+		"code": "Semicolon",
+		"location": 0,
+		"description": "semicolon (firefox), equals"
+	}, {
+		"key": "'",
+		"keyCode": 222,
+		"which": 222,
+		"code": "Quote",
+		"location": 0,
+		"description": "single quote / ø / ä"
+	}, {
+		"key": "Enter",
+		"keyCode": 13,
+		"which": 13,
+		"code": "Enter",
+		"location": 0,
+		"description": "Enter / Return",
+		"unicode": "↵"
+	}
+]
+let fourthRowKeys = [
+	{
+		"key": "Shift",
+		"keyCode": 16,
+		"which": 16,
+		"code": "ShiftLeft",
+		"location": 1,
+		"description": "shift",
+		"unicode": "⇧"
+	},
+	{
+		"key": "\\",
+		"keyCode": 220,
+		"which": 220,
+		"code": "IntlBackslash",
+		"location": 0,
+		"description": "back slash"
+	},
+	{
+		"key": "z",
+		"keyCode": 90,
+		"which": 90,
+		"code": "KeyZ",
+		"location": 0,
+		"description": "z"
+	},
+	{
+		"key": "x",
+		"keyCode": 88,
+		"which": 88,
+		"code": "KeyX",
+		"location": 0,
+		"description": "x"
+	},
+	{
+		"key": "c",
+		"keyCode": 67,
+		"which": 67,
+		"code": "KeyC",
+		"location": 0,
+		"description": "c"
+	},
+	{
+		"key": "v",
+		"keyCode": 86,
+		"which": 86,
+		"code": "KeyV",
+		"location": 0,
+		"description": "v"
+	},
+	{
+		"key": "b",
+		"keyCode": 66,
+		"which": 66,
+		"code": "KeyB",
+		"location": 0,
+		"description": "b"
+	},
+	{
+		"key": "n",
+		"keyCode": 78,
+		"which": 78,
+		"code": "KeyN",
+		"location": 0,
+		"description": "n"
+	},
+	{
+		"key": "m",
+		"keyCode": 77,
+		"which": 77,
+		"code": "KeyM",
+		"location": 0,
+		"description": "m"
+	},
+	{
+		"key": ",",
+		"keyCode": 108,
+		"which": 108,
+		"code": "Comma",
+		"location": 3,
+		"altKey": false,
+		"ctrlKey": false,
+		"metaKey": false,
+		"shiftKey": false,
+		"description": "numpad period (firefox)"
+	},
+	{
+		"key": ".",
+		"keyCode": 190,
+		"which": 190,
+		"code": "Period",
+		"location": 0,
+		"description": "period"
+	},
+	{
+		"key": "/",
+		"keyCode": 191,
+		"which": 191,
+		"code": "Slash",
+		"location": 0,
+		"description": "forward slash / ç"
+	},
+	{
+		"key": "ArrowUp",
+		"keyCode": 38,
+		"which": 38,
+		"code": "ArrowUp",
+		"location": 0,
+		"altKey": false,
+		"ctrlKey": false,
+		"metaKey": false,
+		"shiftKey": false,
+		"repeat": false
+	},
+	{
+		"key": "Shift",
+		"keyCode": 16,
+		"which": 16,
+		"code": "ShiftRight",
+		"location": 1,
+		"description": "shift",
+		"unicode": "⇧"
+	}
+]
+let fifthRowKeys = [
+	{
+		"key": "Control",
+		"keyCode": 17,
+		"which": 17,
+		"code": "ControlLeft",
+		"location": 1,
+		"altKey": true,
+		"ctrlKey": false,
+		"metaKey": true,
+		"shiftKey": true,
+		"description": "ctrl",
+		"unicode": "^"
+	},
+	{
+		"key": "Meta",
+		"keyCode": 91,
+		"which": 91,
+		"code": "MetaLeft",
+		"location": 1,
+		"altKey": false,
+		"ctrlKey": false,
+		"metaKey": true,
+		"shiftKey": false,
+		"description": "Windows Key / Left ⌘ / Chromebook Search key",
+		"unicode": "⌘ ⊞"
+	},
+	{
+		"key": "Alt",
+		"keyCode": 18,
+		"which": 18,
+		"code": "AltLeft",
+		"location": 1,
+		"altKey": true,
+		"ctrlKey": false,
+		"metaKey": true,
+		"shiftKey": false,
+		"description": "Alt / Option",
+		"unicode": "⎇ / ⌥"
+	},
+	{
+		"key": " ",
+		"keyCode": 32,
+		"which": 32,
+		"code": "Space",
+		"location": 0,
+		"altKey": false,
+		"ctrlKey": false,
+		"metaKey": false,
+		"shiftKey": false,
+		"repeat": false
+	},
+	{
+		"key": "Alt",
+		"keyCode": 18,
+		"which": 18,
+		"code": "AltRight",
+		"location": 1,
+		"altKey": true,
+		"ctrlKey": false,
+		"metaKey": true,
+		"shiftKey": false,
+		"description": "Alt / Option",
+		"unicode": "⎇ / ⌥"
+	},
+	{
+		"key": "ArrowLeft",
+		"keyCode": 37,
+		"which": 37,
+		"code": "ArrowLeft",
+		"location": 0,
+		"altKey": false,
+		"ctrlKey": false,
+		"metaKey": false,
+		"shiftKey": false,
+		"repeat": false
+	},
+	{
+		"key": "ArrowDown",
+		"keyCode": 40,
+		"which": 40,
+		"code": "ArrowDown",
+		"location": 0,
+		"altKey": false,
+		"ctrlKey": false,
+		"metaKey": false,
+		"shiftKey": false,
+		"repeat": false
+	},
+	{
+		"key": "ArrowRight",
+		"keyCode": 39,
+		"which": 39,
+		"code": "ArrowRight",
+		"location": 0,
+		"altKey": false,
+		"ctrlKey": false,
+		"metaKey": false,
+		"shiftKey": false,
+		"repeat": false
+	},
+	{
+		"key": "Control",
+		"keyCode": 17,
+		"which": 17,
+		"code": "ControlRight",
+		"location": 1,
+		"altKey": true,
+		"ctrlKey": false,
+		"metaKey": true,
+		"shiftKey": true,
+		"description": "ctrl",
+		"unicode": "^"
+	}
+]
+function init() {
+	document.querySelector('body').innerHTML = '<div class="_container wrapper"><textarea name="textarea" class="textarea" id="textarea" rows="20"></textarea><div id="keyboard" class="keyboard"><div class="keyboard__keys"><div class="row first-row"></div><div class="row second-row"></div><div class="row third-row"></div><div class="row fourth-row"></div><div class="row fifth-row"></div></div></div></div>'
+	let firstRow = document.querySelector('.first-row');
+	let secondRow = document.querySelector('.second-row');
+	let thirdRow = document.querySelector('.third-row');
+	let fourthRow = document.querySelector('.fourth-row');
+	let fifthRow = document.querySelector('.fifth-row');
+	console.log(firstRow);
+	let firstRowButtons = '';
+	let secondRowButtons = '';
+	let thirdRowButtons = '';
+	let fourthRowButtons = '';
+	let fifthRowButtons = '';
+	for (let i = 0; i < firstRowKeys.length; i++) {
+
+		if (firstRowKeys[i].key == 'Backspace') {
+			firstRowButtons += '<div class = "key backspace-key" data = "' + firstRowKeys[i].code + '">' + firstRowKeys[i].key + '</div>';
+		} else {
+			firstRowButtons += '<div class = "key" data = "' + firstRowKeys[i].code + '">' + firstRowKeys[i].key + '</div>';
+		}
+	}
+	for (let i = 0; i < secondRowKeys.length; i++) {
+
+		if (secondRowKeys[i].key == 'Tab') {
+			secondRowButtons += '<div class = "key tab-key" data = "' + secondRowKeys[i].code + '">' + secondRowKeys[i].key + '</div>';
+		} else if (secondRowKeys[i].key == '\\') {
+			secondRowButtons += '<div class = "key slash-key" data = "' + secondRowKeys[i].code + '">' + secondRowKeys[i].key + '</div>';
+		} else if (secondRowKeys[i].key == 'Delete') {
+			secondRowButtons += '<div class = "key delete-key" data = "' + secondRowKeys[i].code + '">Del</div>';
+		} else {
+			secondRowButtons += '<div class = "key" data = "' + secondRowKeys[i].code + '">' + secondRowKeys[i].key + '</div>';
+		}
+	}
+	for (let i = 0; i < thirdRowKeys.length; i++) {
+
+		if (thirdRowKeys[i].key == 'CapsLock') {
+			thirdRowButtons += '<div class = "key capslock-key" data = "' + thirdRowKeys[i].code + '">' + thirdRowKeys[i].key + '</div>';
+		} else if (thirdRowKeys[i].key == 'Enter') {
+			thirdRowButtons += '<div class = "key enter-key" data = "' + thirdRowKeys[i].code + '">' + thirdRowKeys[i].key + '</div>';
+		} else {
+			thirdRowButtons += '<div class = "key" data = "' + thirdRowKeys[i].code + '">' + thirdRowKeys[i].key + '</div>';
+		}
+	}
+	for (let i = 0; i < fourthRowKeys.length; i++) {
+
+		if (fourthRowKeys[i].key == 'Shift') {
+			fourthRowButtons += '<div class = "key shift-key" data = "' + fourthRowKeys[i].code + '">' + fourthRowKeys[i].key + '</div>';
+		} else if (fourthRowKeys[i].key == 'ArrowUp') {
+			fourthRowButtons += '<div class = "key arrow-up-key" data = "' + fourthRowKeys[i].code + '">&#9650;</div>';
+		} else {
+			fourthRowButtons += '<div class = "key" data = "' + fourthRowKeys[i].code + '">' + fourthRowKeys[i].key + '</div>';
+		}
+	}
+	for (let i = 0; i < fifthRowKeys.length; i++) {
+
+		if (fifthRowKeys[i].key == 'Control') {
+			fifthRowButtons += '<div class = "key ctrl-key" data = "' + fifthRowKeys[i].code + '">Ctrl</div>';
+		} else if (fifthRowKeys[i].key == 'Meta') {
+			fifthRowButtons += '<div class = "key win-key" data = "' + fifthRowKeys[i].code + '">Win</div>';
+		}
+		else if (fifthRowKeys[i].key == 'Alt') {
+			fifthRowButtons += '<div class = "key alt-key" data = "' + fifthRowKeys[i].code + '">' + fifthRowKeys[i].key + '</div>';
+		} else if (fifthRowKeys[i].key == ' ') {
+			fifthRowButtons += '<div class = "key space-key" data = "' + fifthRowKeys[i].code + '">' + fifthRowKeys[i].key + '</div>';
+		} else if (fifthRowKeys[i].key == 'ArrowLeft') {
+			fifthRowButtons += '<div class = "key arrow-left-key" data = "' + fifthRowKeys[i].code + '">&#9668;</div>';
+		} else if (fifthRowKeys[i].key == 'ArrowDown') {
+			fifthRowButtons += '<div class = "key arrow-down-key" data = "' + fifthRowKeys[i].code + '">&#9660;</div>';
+		} else if (fifthRowKeys[i].key == 'ArrowRight') {
+			fifthRowButtons += '<div class = "key arrow-right-key" data = "' + fifthRowKeys[i].code + '">&#9658;</div>';
+		} else {
+			fifthRowButtons += '<div class = "key" data = "' + fifthRowKeys[i].code + '">' + fifthRowKeys[i].key + '</div>';
+		}
+	}
+	firstRow.innerHTML = firstRowButtons;
+	secondRow.innerHTML = secondRowButtons;
+	thirdRow.innerHTML = thirdRowButtons;
+	fourthRow.innerHTML = fourthRowButtons;
+	fifthRow.innerHTML = fifthRowButtons;
+}
+init();
+let allKeys = document.querySelectorAll('.key');
+let textarea = document.querySelector('#textarea');
+
+document.onkeydown = function (event) {
+	textarea.focus();
+	allKeys.forEach(function (el) {
+		el.classList.remove('active');
+	})
+	document.querySelector('.key[data="' + event.code + '"]').classList.add('active');
+}
+document.onkeyup = function (event) {
+	document.querySelector('.key[data="' + event.code + '"]').classList.remove('active');
+}
+
+allKeys.forEach(function (item) {
+	item.addEventListener('mousedown', function () {
+		textarea.focus();
+		allKeys.forEach(function (el) {
+			el.classList.remove('active');
+		})
+		let code = this.innerText;
+		// if (this.getAttribute('data') == 'Backspace') {
+
+		// }
+		// console.log(this.getAttribute('data'));
+		this.classList.add('active');
+		textarea.value += code;
+	})
+	item.addEventListener('mouseup', function () {
+		allKeys.forEach(function (el) {
+			el.classList.remove('active');
+		})
+	})
+})
